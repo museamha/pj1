24a91be001ed947389109db973e9e93f414f9eab
museameha <amhamuse173@gmail.com>
2024-11-10 20:02:07 +0300

    i fixed the issue that was causing my project to fail

--- cars/urls.py
+++ cars/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("",views.FrontPageView.as_view(), name="front-page"),
-    path('posts/<slug:slug>/', views.SinglePageView.as_view, name='detail-page'),
-    path("read-later", views.ReadLaterView.as_view(), name="read-later")
+    path("cars/<slug:slug>/",views.SinglePageView.as_view(), name="detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later"),
 ]
