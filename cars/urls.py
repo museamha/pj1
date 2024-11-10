2852fcb9410fbe3e1da0b902d62a11bd5570c565
museameha <amhamuse173@gmail.com>
2024-11-10 19:18:46 +0300

    just styling and fixing the add to cart system

--- cars/urls.py
+++ cars/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("",views.FrontPageView.as_view(), name="front-page"),
-    path("posts/<slug:slug>", views.SinglePageView.as_view(),
-         name="detail-page"),
+    path('posts/<slug:slug>/', views.SinglePageView.as_view, name='detail-page'),
     path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
