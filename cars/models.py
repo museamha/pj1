81f939cfb627e0ad0a7e84c41c88ad8ae876fbe5
museameha <amhamuse173@gmail.com>
2024-11-11 18:57:31 +0300

    updated the relation

--- cars/models.py
+++ cars/models.py
@@ -8,6 +8,13 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Producer(models.Model):
+    name = models.CharField(max_length=20)
+    logo = models.ImageField(upload_to="posts", null=True)
+
+    def __str__(self):
+        return self.name
+
 class car(models.Model):
     Proname = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
@@ -18,19 +25,16 @@ class car(models.Model):
     discr = models.TextField(default='')
     sped = models.IntegerField(validators=[MaxValueValidator(250)], default=0)
     slug = models.SlugField(unique=True, db_index=True)
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE, related_name='cars', null=True, blank=True, default=None)  # ForeignKey placed here
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.Proname)
         super().save(*args, **kwargs)
+
     def __str__(self):
         return self.Proname
+
     class Meta:
         verbose_name = "Car"
         verbose_name_plural = "Cars"
-
-class Producer(models.Model):
-    name = models.CharField(max_length=20)
-    logo = models.ImageField(upload_to="posts", null=True)
-    carsp = models.ForeignKey(car, verbose_name="Car", on_delete=models.CASCADE, blank=True, null=True)
-    def __str__(self):
-        return self.name
