66dcaf7c24aca66e96d84d4b642163d9ab161d05
museameha <amhamuse173@gmail.com>
2024-11-12 19:28:26 +0300

    just traying tings

--- cars/models.py
+++ cars/models.py
@@ -12,6 +12,7 @@ class Producer(models.Model):
     name = models.CharField(max_length=20)
     logo = models.ImageField(upload_to="posts", null=True)
 
+
     def __str__(self):
         return self.name
 
